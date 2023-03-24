a646f96a1bbb635f569628ec68c1d0a815faa0be
maych3n <cheny197@newschool.edu>
2023-03-24 09:05:02 -0400

    udate

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ console.log('Remove this console log and get started!')
 
 const data = {
   channels: [
-    // Add your channel data here
-    // {
-    //   title: 'UFOs',
-    //   videoIds: [
-    //     // Add your video IDs here
-    //   ]
-    // }
-  ]
+    {
+        title: 'travel',
+        videoIds: ['canada', 'italy', 'thailand']
+        },
+   
+  {
+        title: 'food',
+        videoIds: ['pasta', 'pizza', 'cake']
+        },
+
+        {
+          title: 'activities',
+          videoIds: ['yoga', 'painitng', 'basketball']
+          },  
+     ]
 }
+
+// channels.forEach(selectvideo);
+// function selectvideo(){
+  let buttonone = document.createElement('button');
+  buttonone.innerHTML='travel';
+
+  let channels = document.getElementById ('channels');
+  channels.appendChild(buttonone);
+
+// }
+
+buttonone.addEventListener('click', function(){
+data.channels.forEach(channels,index,arr)
+});
